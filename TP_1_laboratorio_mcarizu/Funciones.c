da309626def7ac5c37285dc9fb8b7c35918ac869
mcarizu <carizumatias@gmail.com>
2018-04-11 20:09:14 -0300

    Se cambiaron lo continue por los break

--- TP_1_laboratorio_mcarizu/Funciones.c
+++ TP_1_laboratorio_mcarizu/Funciones.c
@@ -65,35 +65,36 @@ void menu(void){
 
 
     while(op<'1'||op>'9'){
-            op = pedirDatos("\t\t\tIngrese un valor 1-6\n1)Ingresar 1er operando A=x\n2)Ingresar 2do operando B=y\n3)Calcular la Suma\n4)Calcular la Resta\n5)Calcular la Multiplicacion\n6)Calcular la Divicion\n7)Calcular el Factorial\n8)Todos las operaciones\n9)\nSalir\n", 1,  9);
+             printf("\t\t\tIngrese un valor 1-6\n1)Ingresar 1er operando A=x\n2)Ingresar 2do operando B=y\n3)Calcular la Suma\n4)Calcular la Resta\n5)Calcular la Multiplicacion\n6)Calcular la Divicion\n7)Calcular el Factorial\n8)Todos las operaciones\n9)\nSalir\n", 1,  9);
+             op =scanf("d",&op);
          switch(op){
 
             case 1 :x=ingresarNumeros("Ingrese un Valor para A\n",x);
                     printf("\nEl valor de A :%d\n",x);
-                    continue;
+                    break;
             case 2 :y=ingresarNumeros("Ingrese un Valor para B\n",y);
                     printf("\nEl valor de B :%d\n",y);
-                    continue;
+                    break;
             case 3 :printf("\nLa Suma...\n");
                     total = sumar(x, y);
                     printf("\nEl Resultado de la suma es: %d\n",total);
-                    continue;
+                    break;
             case 4 :printf("\nPara la Resta...\n");
                     total=restar(x, y);
                     printf("\nEl Resultado de la resta es: %d\n",total);
-                    continue;
+                    break;
             case 5 :printf("\nPara la Multiplicar...\n");
                     total=multiplicar(x, y);
                     printf("\nEl Resultado de la multiplicacion es: %d\n",total);
-                    continue;
+                    break;
             case 6 :printf("\nPara la Dividir...\n");
                     total=dividir(x, y);
                     printf("\nEl Resultado de la divicion: %d\n",total);
-                    continue;
+                    break;
             case 7 :printf("\nPara el Factorial...\n");
                     total=factorial(x);
 
-                    continue;
+                    break;
             case 8: printf("Calcular todas las operaciones");
                 break;
             case 9 : printf("Salir");
