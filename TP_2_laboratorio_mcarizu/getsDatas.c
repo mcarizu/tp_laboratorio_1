e38441cd0634018dd0e094347373a7d663ccf0fa
mcarizu <carizumatias@gmail.com>
2018-05-12 01:09:40 -0300

    VER 2.0

---
+++ TP_2_laboratorio_mcarizu/getsDatas.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/*
+float getFloat(char mensaje[])
+{
+    float auxiliar;
+    printf("%s",mensaje);
+    scanf("%f",&auxiliar);
+    return auxiliar;
+}
+
+int getInt(char mensaje[])
+{
+    int auxiliar;
+    printf("%s",mensaje);
+    scanf("%d",&auxiliar);
+    return auxiliar;
+}
+
+char getChar(char mensaje[])
+{
+    char auxiliar;
+    printf("%s",mensaje);
+    fflush(stdin);
+    scanf("%c",&auxiliar);
+    return auxiliar;
+}
+*/
